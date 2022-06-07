4e33b7860a30c46b822f42ed2456b1a8782d207c
avsha114 <avsha114@gmail.com>
2022-06-07 13:51:25 +0300

    fixed instructions

--- webapp/main.py
+++ webapp/main.py
@@ -63,6 +63,6 @@ def apply():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
